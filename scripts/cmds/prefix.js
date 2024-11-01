20883a53372c85c7f3be349840c1235906bb50c5
Robiulislame <robiulislamsohid2@gmail.com>
2024-11-01 14:58:16 +0600

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -15,13 +15,13 @@ module.exports = {
  if (event.body && event.body.toLowerCase() === "prefix") {
  return message.reply({
  body: `
-𝗠𝗬 𝗣𝗥𝗘𝗙𝗜𝗫 [.]\n
+𝗠𝗬 𝗣𝗥𝗘𝗙𝗜𝗫 [/]\n
 𝗦𝗢𝗠𝗘 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 𝗧𝗛𝗔𝗧 𝗠𝗔𝗬 𝗛𝗘𝗟𝗣 𝗬𝗢𝗨:
 ➥. help [number of page] -> see commands
 ➥. sim [message] -> talk to bot
 ➥. callad [message] -> report any problem encountered
-➥. help [command] -> information and usage of command\n\n𝗛𝗮𝘃𝗲 𝗳𝘂𝗻 𝘂𝘀𝗶𝗻𝗴 𝗶𝘁!❤️\n𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥 𝗠𝗗 𝗝𝗨𝗕𝗔𝗘𝗗 𝗔𝗛𝗠𝗘𝗗 𝗝𝗢𝗬\n𝗪𝗛𝗔𝗧𝗦 𝗔𝗣𝗣: 01709045888\n\n𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞: https://www.facebook.com/profile.php?id=100001741442385`,
- attachment: await global.utils.getStreamFromURL("https://i.imgur.com/82re01F.jpeg")
+➥. help [command] -> information and usage of command\n\n𝗛𝗮𝘃𝗲 𝗳𝘂𝗻 𝘂𝘀𝗶𝗻𝗴 𝗶𝘁!❤️\n🇧 🇴 🇹♥️🇴 🇼 🇳 🇪 🇷♥️🇷 🇴 🇧 🇮 🇺 🇱 \n𝗪𝗛𝗔𝗧𝗦 𝗔𝗣𝗣: 01709045888\n\n𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞: https://www.facebook.com/profile.php?id=100001741442385`,
+ attachment: await global.utils.getStreamFromURL("https://i.imgur.com/ZFCzZjW.jpeg")
  });
  }
  }
