f398cd93546c47e8dd25d3421ff4b03b16d4a980
Robiulislame <robiulislamsohid2@gmail.com>
2024-11-04 06:35:56 +0600

    Update and rename Joy.js to robiul.js

--- scripts/cmds/robiul.js
+++ scripts/cmds/robiul.js
@@ -1,6 +1,6 @@
  module.exports = {
  config: {
-	 name: "Joy",
+	 name: "robiul",
 	 version: "1.0",
 	 author: "AceGun",
 	 countDown: 5,
@@ -12,10 +12,10 @@
 
  onStart: async function(){}, 
  onChat: async function({ event, message, getLang }) {
- if (event.body && event.body.toLowerCase() === "Joy") {
+ if (event.body && event.body.toLowerCase() === "robiul") {
  return message.reply({
  body: "জয় বস কাজে বিজি আছেন",
- attachment: await global.utils.getStreamFromURL("https://i.imgur.com/XXANp1W.jpeg")
+ attachment: await global.utils.getStreamFromURL("")
  });
  }
  }
